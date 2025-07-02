f15878375f6030ec7134c48f939cec65c42d3990
Hardip Singh <hardipsingh@hardipsingh-mac.local>
2025-07-02 02:08:26 -0400

    Fix syntax error in console.log

--- proxy-server.js
+++ proxy-server.js
@@ -46,6 +46,6 @@ app.get('/api/recommend', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Proxy server running at http://localhost:' + PORT);}\`);
+  console.log('Proxy server running at http://localhost:' + PORT);`);
 });
   # adds a harmless empty line
